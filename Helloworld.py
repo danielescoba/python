8b18639ce185167a2ba3de56d9882e9684cb0ea1
Daniel Escobar <68076771+danielescoba@users.noreply.github.com>
2020-12-27 23:48:56 -0500

    update

---
+++ Helloworld.py
@@ -0,0 +1,14 @@
+print ("Hello World")
+
+name = "Noelle"
+print ("Hello",name+"!")
+print ("Hello " + name + "!")
+
+name = 42
+print ("Hello", str(name) + "!")
+print ("Hello " + str(name)+"!")
+
+fave_food1="sushi"
+fave_food2="pizza"
+print("I love to eat %s and %s" % (fave_food1, fave_food2))
+print(f"I love to eat {fave_food1} and {fave_food2}")
